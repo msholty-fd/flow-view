885097b7fc272ce9e7e72c2eed54f1fd1d5b7528
fibo <casati_gianluca@yahoo.it>
2016-06-06 10:24:45 +0200

    refactored directory structure

---
+++ _includes/jscode/basic/usage.js
@@ -0,0 +1,28 @@
+var Canvas = require('flow-view').Canvas
+var view = {
+  width: 400, height: 300,
+  node: {
+    a: {
+      x: 80, y: 100,
+      width: 100,
+      text: 'Drag me',
+      outs: ['out1', 'out2', 'out3']
+    },
+    b: {
+      x: 180, y: 200,
+      text: 'Click me',
+      ins: ['in0', { name: 'in1', type: 'bool' }],
+      outs: ['return']
+    }
+  },
+  link: {
+    c: {
+      from: ['a', 0],
+      to: ['b', 1]
+    }
+  }
+}
+
+var canvas = new Canvas('drawing')
+
+canvas.render(view)
